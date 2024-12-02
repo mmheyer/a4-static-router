f5a6aff102b1fa7b0df2e5b19d0ffed251649520
cece-cuevas <ceceliaa@umich.edu>
2024-12-02 12:43:48 -0500

    Added code for handleIP in StaticRouter.cpp & .h

--- src/StaticRouter.h
+++ src/StaticRouter.h
@@ -24,12 +24,17 @@ public:
     void handlePacket(std::vector<uint8_t> packet, std::string iface);
 
 private:
+    void handleIP(std::vector<uint8_t>& packet, const std::string& iface, sr_ethernet_hdr_t* ethHeader);
+    void forwardIPPacket(std::vector<uint8_t>& packet, const std::string& iface, sr_ethernet_hdr_t* ethHeader, sr_ip_hdr_t* ipHeader);
+    void handleICMPEchoRequest(std::vector<uint8_t>& packet, const std::string& iface, sr_ethernet_hdr_t* ethHeader);
+
     std::mutex mutex;
+    std::shared_ptr<IRoutingTable> routingTable; // Routing table for forwarding decisions
+    std::shared_ptr<IPacketSender> packetSender; // Interface for sending packets
+    std::unique_ptr<IArpCache> arpCache; // ARP cache for resolving ip-to-mac mappings
 
-    std::shared_ptr<IRoutingTable> routingTable;
-    std::shared_ptr<IPacketSender> packetSender;
-
-    std::unique_ptr<IArpCache> arpCache;
+    std::shared_ptr<ICMPSender> icmpSender; // Sends ICMP error and echo reply msgs
+    std::shared_ptr<ARPSender> arpSender; // Sends ARP reqs and replies.
 };
 
 
