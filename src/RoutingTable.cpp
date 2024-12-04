9a59e0ef76e56eebca4ad446e40f79b310965646
cece-cuevas <ceceliaa@umich.edu>
2024-12-03 22:16:40 -0500

    Fixed RoutingTable.cpp Again

--- src/RoutingTable.cpp
+++ src/RoutingTable.cpp
@@ -40,8 +40,7 @@ RoutingTable::RoutingTable(const std::filesystem::path& routingTablePath) {
 // Called when our router needs to determine how to forward an IP packet
 std::optional<RoutingEntry> RoutingTable::getRoutingEntry(ip_addr ip) {
     uint32_t bestMatchLength = 0;  // len of longest matching prefix, initially 0
-    std::optional<RoutingEntry> bestEntry = std::nullopt; // initially, no match 
-    //uint32_t bestCloseness = UINT32_MAX; // used for ties
+    std::optional<RoutingEntry> bestEntry = std::nullopt; // initially, no match
     spdlog::info("Starting getRoutingEntry for IP: {:#010x}", ip);
 
     // for all entries in the routing table
@@ -51,20 +50,16 @@ std::optional<RoutingEntry> RoutingTable::getRoutingEntry(ip_addr ip) {
         uint32_t maskedDest = entry.dest & entry.mask; // bitwise & desination IP and mask
         uint32_t maskedIP = ntohl(ip) & entry.mask; // bitwise & input ip and mask
         spdlog::info("Masked destination: {:#010x}, Masked input: {:#010x}", maskedDest, maskedIP);
+        
         // if maskedDest == maskedIP, the input ip falls within the entry's subnet mask
         // therefore, the input ip belongs to the same network as the routing entry
         if (maskedDest == maskedIP) {
             // count num of bits set to 1 (prefix length) in the subnet mask
-            uint32_t maskLength = __builtin_popcount(ntohl(entry.mask)); // convert mask using network to host long for popcount
-            //uint32_t maskLength = __builtin_popcount(entry.mask);
-            // resolve ties by prefering the entry where the input IP is closer to the entry's destination
-            //uint32_t closeness = ntohl(ipNetworkOrder ^ maskedDest); // smaller val => closer match
-            //uint32_t closeness = ip ^ maskedDest;
+            uint32_t maskLength = __builtin_popcount(entry.mask); // convert mask using network to host long for popcount
             spdlog::info("Match found: maskLength={}", maskLength);
-            // update best match if:
-            // 1. prefix length is longer OR
-            // 2. (TOOK THIS PART OUT, NOT NEEDED) prefix length is the same, but the closeness is smaller
-            if (maskLength > bestMatchLength) {  // || (maskLength == bestMatchLength && closeness < bestCloseness)) {
+            
+            // update best match if prefix length is longer
+            if (maskLength > bestMatchLength) { 
                 spdlog::info("New best match: dest={:#010x}, mask={:#010x}, gateway={:#010x}, iface={}", entry.dest, entry.mask, entry.gateway, entry.iface);
                 bestMatchLength = maskLength;
                 bestEntry = entry;
