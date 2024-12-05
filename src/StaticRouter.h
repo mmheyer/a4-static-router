eb6a999c0100e0f11e16681d5ab9cdebbd68ba85
areenac <156745436+areenac@users.noreply.github.com>
2024-12-04 20:54:48 -0500

    Update StaticRouter.h

--- src/StaticRouter.h
+++ src/StaticRouter.h
@@ -7,7 +7,8 @@
 #include "IArpCache.h"
 #include "IPacketSender.h"
 #include "IRoutingTable.h"
-
+#include "ICMPSender.h"
+#include "ARPSender.h"
 
 
 class StaticRouter {
@@ -28,13 +29,19 @@ private:
     void handleIP(std::vector<uint8_t>& packet, const std::string& iface, sr_ethernet_hdr_t* ethHeader);
     void forwardIPPacket(std::vector<uint8_t>& packet, const std::string& iface, sr_ethernet_hdr_t* ethHeader, sr_ip_hdr_t* ipHeader);
     void handleICMPEchoRequest(std::vector<uint8_t>& packet, const std::string& iface, sr_ethernet_hdr_t* ethHeader);
+    void sendDestinationUnreachable(const std::vector<uint8_t>& packet,
+                                            const std::string& iface,
+                                            uint32_t sourceIP,
+                                            uint8_t icmpCode);
+uint8_t getICMPType(const std::vector<uint8_t>& packetData);
 
     std::mutex mutex;
     std::shared_ptr<IRoutingTable> routingTable; // Routing table for forwarding decisions
     std::shared_ptr<IPacketSender> packetSender; // Interface for sending packets
     std::unique_ptr<IArpCache> arpCache; // ARP cache for resolving ip-to-mac mappings
 
-    
+    std::shared_ptr<ICMPSender> icmpSender; // Sends ICMP error and echo reply msgs
+    std::shared_ptr<ARPSender> arpSender;
 };
 
 
